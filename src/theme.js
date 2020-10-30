e0ab01d410f9a4fe14dce3b93eb0fdfb0b99e0f1
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-10-29 18:45:27 -0700

    changed border of outlined buttons to primary on hover

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,13 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 require('typeface-open-sans');
 
+const primary = '#184d47';
 const secondary = '#96bb7c';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#184d47',
+      main: primary,
     },
     secondary: {
       main: secondary,
@@ -43,8 +44,9 @@ const theme = createMuiTheme({
         border: '2px solid #96bb7c',
         fontWeight: 600,
         '&:hover': {
-          border: '2px solid #96bb7c',
+          border: `2px solid ${primary}`,
           boxShadow: '0px 2px 2px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.04),0px 1px 1px 0px rgba(0,0,0,0.12)',
+          color: primary,
         },
       },
       containedPrimary: {
