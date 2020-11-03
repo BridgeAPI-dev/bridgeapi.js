3fe46fd1083897baa4922843d83de63696980d82
= <awcrotwell@gmail.com>
2020-11-03 15:23:28 -0500

    Basic code mirror implementation

--- components/editor/PayloadCard/index.js
+++ components/editor/PayloadCard/index.js
@@ -1,7 +1,14 @@
+/* eslint-disable global-require */
 import {
   Accordion, AccordionDetails, Container, Typography, makeStyles, AccordionSummary as MUIAccordionSummary,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import { JSHINT } from 'jshint';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/addon/lint/lint.css';
 
 import AccordionSummary from '../../AccordionSummary';
 
@@ -22,8 +29,29 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Payload() {
+function Payload({ inputCode }) {
   const classes = useStyles();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+    // Must bind JSHINT to window for lint support
+    window.JSHINT = JSHINT;
+    // Requring this at the top level causes
+    // `ReferenceError: navigator is not defined` because
+    // `navigator` is a react DOM thing and is not avaiable on the server
+    require('codemirror/mode/javascript/javascript');
+    require('codemirror/addon/lint/lint');
+    require('codemirror/addon/lint/javascript-lint');
+  }, []);
+
+  const code = inputCode
+    || '// Javascript Object Syntax\n'
+      + '{\n  '
+      + 'hello: "world"\n  '
+      + 'env: $keys.MY_KEY\n  '
+      + 'payload: $payload.status\n'
+      + '}';
+
   return (
     <Accordion defaultExpanded>
       <AccordionSummary
@@ -52,6 +80,31 @@ function Payload() {
           </Accordion>
 
           <Typography>Edit outbound payload</Typography>
+          {mounted
+            && (
+            <CodeMirror
+              value={code}
+              options={{
+                mode: {
+                  name: 'javascript',
+                  json: false,
+                  statementIndent: 2,
+                  esversion: 6,
+                },
+                theme: 'material',
+                lineNumbers: true,
+                lint: true,
+                tabSize: 2,
+                autocomplete: true,
+                lineWrapping: true,
+                indentWithTabs: false,
+              }}
+              onChange={(editor, data, value) => {
+                console.log(value);
+              }}
+            />
+            )}
+
         </Container>
       </AccordionDetails>
     </Accordion>
