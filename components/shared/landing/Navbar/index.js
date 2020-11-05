c27288192c95406d020790c8863c2f1fa6ba35af
Andrew <49383564+andrewc910@users.noreply.github.com>
2020-11-05 11:34:21 -0500

    WIP - Landing Page (#23)
    * Starting on homepage
    
    * Working on landing page
    
    * Colored containers with bottom CTA

---
+++ components/shared/landing/Navbar/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Link from 'next/link';
+import {
+  AppBar, Toolbar, Typography, makeStyles, Grid,
+} from '@material-ui/core';
+import logo from '../../../../src/images/logo.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: '50px',
+  },
+  appbar: {
+    alignItems: 'right',
+    backgroundColor: 'white',
+    color: 'rgb(80, 82, 86)',
+    flexGrow: 1,
+  },
+  toolbar: {
+    minHeight: 46,
+    flexGrow: 1,
+  },
+  login: {
+    marginRight: theme.spacing(1),
+    padding: '5px',
+    '&:hover': {
+      color: '#000',
+    },
+  },
+  signup: {
+    backgroundColor: '#f1f2f4',
+    borderRadius: '5px',
+    paddingRight: '5px',
+    paddingLeft: '5px',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    '&:hover': {
+      color: '#000',
+    },
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appbar}>
+        <Toolbar className={classes.toolbar}>
+          <Link href="/">
+            <img src={logo} width={150} />
+          </Link>
+
+          <Grid container justify="flex-end">
+            <Typography variant="subtitle1" className={classes.login}>
+              <Link href="/users/login">
+                Login
+              </Link>
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              className={classes.signup}
+            >
+              <Link href="/users/signup">
+                Sign Up
+              </Link>
+            </Typography>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,6 @@ import Footer from '../components/shared/Footer';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    background: 'white',
   },
   'mt-2': {
     marginTop: theme.spacing(2),
@@ -18,15 +17,6 @@ const useStyles = makeStyles((theme) => ({
   },
   'mt-6': {
     marginTop: theme.spacing(6),
-  },
-  'm-4': {
-    margin: theme.spacing(4),
-  },
-  'm-6': {
-    margin: theme.spacing(6),
-  },
-  manage: {
-    backgroundColor: '#f6f7f8',
   },
 }));
 
@@ -59,55 +49,43 @@ export default function Home() {
                 Get Started
               </Button>
             </Link>
-            <Typography variant="subtitle2" className={classes['m-6']}>
+            <Typography variant="subtitle2" className={classes['mt-6']}>
               Join the developers that are integrating API&#39;s around the world.
             </Typography>
           </Grid>
         </Grid>
-      </Container>
 
-      <Container maxWidth="lg" className={classes.manage}>
         <Grid container spacing={3}>
-          <Grid container item xs={12}>
-            <Typography variant="h5" component="h2" className={classes['mt-2']}>
-              Some Title
+          <Grid container item xs={12} justify="center">
+            <Typography variant="h3" component="h1" className={classes['mt-6']}>
+              Bridge Incompatible API&#39;s
             </Typography>
           </Grid>
           <Grid container item xs={12} alignItems="center" direction="column">
             <Typography variant="subtitle1">
-              Maybe a 2 x 2 grid with some icons and light text on the right side
+              BridgeAPI is a serverless integration platform that
             </Typography>
             <Typography variant="subtitle1">
-              dont ask me what to do for this box. I am out of ideas.
-            </Typography>
-
-            <Typography variant="subtitle2" className={classes['mt-6']}>
-              Some small test here
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-
-      <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid container item xs={12} justify="center">
-            <Typography variant="h5" component="h2" className={classes['m-4']}>
-              Some Call To Action
+              empowers users to connect apps through event-driven workflows
             </Typography>
             <Link href="/users/signup">
               <Button
                 color="primary"
                 variant="contained"
-                className={classes['m-4']}
+                className={classes['mt-6']}
               >
-                Get Started Now
+                Get Started
               </Button>
             </Link>
+            <Typography variant="subtitle2" className={classes['mt-6']}>
+              Join the developers that are integrating API&#39;s around the world.
+            </Typography>
           </Grid>
         </Grid>
-      </Container>
 
+      </Container>
       <Footer />
+
     </div>
   );
 }
