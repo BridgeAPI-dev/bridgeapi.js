9fc78ead56f5acd97829ff1fea42155343b83b9d
= <awcrotwell@gmail.com>
2020-11-05 10:13:40 -0500

    Fix height of navbar

--- components/shared/dashboard/Navbar/Menu.js
+++ components/shared/dashboard/Navbar/Menu.js
@@ -34,7 +34,7 @@ function Menu({
         aria-haspopup="true"
         onClick={handleMenu}
         color="inherit"
-        style={{ right: (width / 2) }}
+        style={{ right: (width / 2), padding: '0 !important' }}
         className={classes.iconButton}
       >
         {text}
