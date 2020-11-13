87d7d4e7d1b5df1f9dbc5bbf3a620fd1d07b10c8
= <awcrotwell@gmail.com>
2020-11-13 08:54:55 -0500

    Working on CRUD operations for bridges

---
+++ pages/bridge/[id].js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import Editor from '../../components/Editor';
+import ProtectRoute from '../../utils/ProtectRoute';
+import fetchDataOrRedirect from '../../utils/ssrRedirect';
+
+import toCamel from '../../utils/toCamel';
+
+function Show({ bridge }) {
+  const {
+    outboundUrl, method, retries, delay, headers, environmentVariables,
+  } = bridge;
+
+  return (
+    <ProtectRoute>
+      <Editor
+        outboundURL={outboundUrl}
+        method={method}
+        retries={retries}
+        delay={delay}
+        headers={headers}
+        envVars={environmentVariables}
+        isEditView
+      />
+    </ProtectRoute>
+  );
+}
+
+export default Show;
+
+export async function getServerSideProps(context) {
+  const res = await fetchDataOrRedirect(context, `/bridges/${context.query.id}`);
+  if (!res) return { props: {} }; // Redirecting to /users/login
+
+  return {
+    props: {
+      bridge: toCamel(res.data.bridge),
+    },
+  };
+}
+
+Show.propTypes = {
+  bridge: PropTypes.shape({
+    outboundUrl: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+    retries: PropTypes.string.isRequired,
+    delay: PropTypes.string.isRequired,
+    headers: PropTypes.arrayOf(
+      PropTypes.shape({
+        key: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    environmentVariables: PropTypes.arrayOf(
+      PropTypes.shape({
+        key: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+  }).isRequired,
+};
