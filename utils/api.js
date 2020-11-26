e5e318050b2b85121bf9a39cdd34d1635835df9a
Andrew <49383564+andrewc910@users.noreply.github.com>
2020-11-26 11:58:12 -0600

    Sign Up & Login Specs (#50)
    * Working on sign up specs
    
    * Clean up stubbing
    
    * Cypress specs'
    
    * Integrate mock service worker for stubbing node requests
    
    * Support cypress stubbing with #intercept
    
    * Trying to get intercept to work with getServerSideProps
    
    * Move specs to specs folder, configure dynamic msw stubs
    
    * DRY loging & sign_up specs
    
    * Remove cypress examples
    
    * Fix eslint errors
    
    * Break login_signup helpers into more generic files
    
    * Add missing spec for sign up

--- utils/api.js
+++ utils/api.js
@@ -1,12 +1,22 @@
 import Axios from 'axios';
 
 const urls = {
-  test: 'http://localhost:3001',
   development: 'http://localhost:3001/',
-  production: 'http://localhost:3001/',
+  production: 'http://localhost:3004/',
 };
+
+// Tests require the application to be built and
+// ran with `npm run start` which ALWAYS sets the
+// NODE_ENV to `production`. NEXT_PUBLIC_TEST_ENV
+// is designed to be used in a test environment.
+// This ternary was a solution for tests without
+// making breaking changes to dev/production behavior.
+const url = process.env.NEXT_PUBLIC_TEST_ENV
+  ? ''
+  : urls[process.env.NODE_ENV];
+
 const api = Axios.create({
-  baseURL: urls[process.env.NODE_ENV],
+  baseURL: url,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
