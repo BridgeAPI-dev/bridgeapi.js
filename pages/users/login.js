f5d9c935d8a901c782657e4420f7548aa47c729d
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-12-06 16:36:54 -0400

    fixes login errors

--- pages/users/login.js
+++ pages/users/login.js
@@ -180,8 +180,8 @@ function Login() {
 
         </Container>
       </Paper>
-      <SnackAlert open={successOpen} onClose={handleSnackClose} severity="success" message="Success: Logging in. Please wait." />
-      <SnackAlert open={errorOpen} onClose={handleSnackClose} severity="error" message="Error: Email or password is invalid" />
+      <SnackAlert id="login_success_alert" open={successOpen} onClose={handleSnackClose} severity="success" message="Success: Logging in. Please wait." />
+      <SnackAlert id="login_fail_alert" open={errorOpen} onClose={handleSnackClose} severity="error" message="Error: Email or password is invalid" />
     </Grid>
   );
 }
