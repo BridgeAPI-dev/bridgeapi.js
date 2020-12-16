99fd8c55b79e316942a14a85a4ae71ff40845b2e
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-12-15 20:02:52 -0400

    added media queries

--- components/About/William.js
+++ components/About/William.js
@@ -1,12 +1,21 @@
 import { Avatar, Link, makeStyles } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+// import PublicIcon from '@material-ui/icons/Public';
 import EmailIcon from '@material-ui/icons/Email';
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
     width: '100%',
-    height: '30vh',
     marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('xs')]: {
+      height: '12vh',
+    },
+    [theme.breakpoints.up('md')]: {
+      height: '25vh',
+    },
+    [theme.breakpoints.up('lg')]: {
+      height: '30vh',
+    },
   },
   link: {
     margin: theme.spacing(0, 2),
