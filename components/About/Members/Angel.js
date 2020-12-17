9bf0847c88f1054f0cc4087c55b28ea82df9d727
William Jackson <williampj@gmail.com>
2020-12-17 15:28:08 -0600

    eslint error fix

--- components/About/Members/Angel.js
+++ components/About/Members/Angel.js
@@ -14,11 +14,7 @@ export default function Angel() {
       <Typography variant="subtitle1">Software Engineer</Typography>
       <Typography variant="subtitle2">Rincón, PR</Typography>
       <Links linkedIn="https://www.linkedin.com/in/angel-ruiz-bates-1b68a2142/" email="angelbates5@yahoo.com" />
-<<<<<<< HEAD
-      <AdditionalInfo interests="Full-stack development, decentralized systems, security, AI, containerization, algorithms." />
-=======
       <AdditionalInfo interests="Full-stack development, decentralized systems, security, AI, containerization, algorithms" />
->>>>>>> b4243f3dc50d3f2120e22ce7a9002717f903d4fa
     </>
   );
 }
