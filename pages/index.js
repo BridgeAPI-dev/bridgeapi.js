9b9ee68ef28ce6adaf269908343a5d6cee39cabd
angeljruiz <angelbates5@yahoo.com>
2020-12-23 19:02:51 -0400

    Landing page update (#83)
    * started landing page
    
    * updated pic, added some margins
    
    * Finished landing page
    
    * shortening text so image is stretched less
    
    * added links to buttons
    
    * text changes
    
    * make landing page responsive
    
    * small padding for & margin changes
    
    * text changes
    
    * padding change
    
    * text change

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,67 @@
 import React, { useState } from 'react';
 import {
-  Card, Grid, Button, Container, Typography, Link, makeStyles,
+  Button,
+  Container,
+  Grid,
+  makeStyles, Typography,
 } from '@material-ui/core';
 
+import Link from 'next/link';
 import Navbar from '../components/shared/landing/Navbar';
-import Footer from '../components/shared/Footer';
 import ContactForm from '../components/ContactModal';
+import dashboard from '../public/dashboard.png';
+import headers from '../public/headers.png';
+import environmentVariables from '../public/environment variables.png';
+import payload from '../public/payload.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     background: 'white',
   },
-  'mt-2': {
-    marginTop: theme.spacing(2),
+  main: {
+    backgroundColor: theme.palette.offWhite.main,
+    padding: theme.spacing(10, 0),
   },
-  'mt-4': {
-    marginTop: theme.spacing(4),
+  blue: {
+    backgroundColor: theme.palette.offWhite.main,
+    padding: theme.spacing(15, 10),
+    [theme.breakpoints.down('md')]: {
+      padding: theme.spacing(15, 5),
+    },
   },
-  'mt-6': {
-    marginTop: theme.spacing(6),
-    fontWeight: 'bold',
+  header: {
+    fontWeight: 700,
+    color: theme.palette.primary.main,
+    marginBottom: theme.spacing(5),
   },
-  'm-4': {
-    margin: theme.spacing(4),
+  mainText: {
+    fontSize: '1.2rem',
+    marginBottom: theme.spacing(5),
   },
-  'm-6': {
-    margin: theme.spacing(6),
+  button: {
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+    fontWeight: '600',
+    fontSize: '1.1rem',
+    borderRadius: theme.spacing(1.25),
+    padding: theme.spacing(1.2, 3.5),
+    marginBottom: theme.spacing(6),
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.main,
+    },
   },
-  manage: {
-    backgroundColor: '#f6f7f8',
+  paddedContainer: {
+    padding: theme.spacing(15, 0),
+    margin: theme.spacing(0, 10),
+    [theme.breakpoints.down('md')]: {
+      margin: theme.spacing(0, 5),
+    },
+  },
+  environmentVariablesImage: {
+    [theme.breakpoints.down('md')]: {
+      marginBottom: theme.spacing(5),
+    },
   },
 }));
 
@@ -37,118 +69,136 @@ export default function Home() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
   return (
     <div className={classes.root}>
       <Navbar />
       <ContactForm open={open} setOpen={setOpen} />
-
-      <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid container item xs={12} justify="center">
-            <Typography variant="h3" component="h1" className={classes['mt-6']}>
-              Bridge incompatible API&#39;s
-            </Typography>
+      <Container maxWidth="xl" style={{ padding: 0 }}>
+        <Grid container justify="center" align="center" className={classes.main}>
+          <Grid container item xs={8} justify="center">
+            <Typography variant="h2" className={classes.header}>Bridge Incompatible API&apos;s</Typography>
           </Grid>
-
-          <Grid container item xs={12} alignItems="center" direction="column">
-            <Typography variant="subtitle1">
+          <Grid container item xs={10} md={6}>
+            <Typography variant="body1" className={classes.mainText}>
               BridgeAPI is a serverless integration platform that
-            </Typography>
+              lets you connect your apps through event-driven workflows.
+              <br />
+              <br />
+              Current options
+              for developers usually require subscribing to bloated services and granting
+              intrusive permissions in order to connect apps.
+              <br />
+              <br />
+              With BridgeAPI, simply define
+              your desired workflow and direct your vendor&apos;s webhooks or API calls towards
+              your bridge&apos;s provided endpoint.
 
-            <Typography variant="subtitle1">
-              lets you connect your apps through event-driven workflows
             </Typography>
-
+          </Grid>
+          <Grid item xs={12} align="center">
             <Link href="/users/signup">
-              <Button
-                color="primary"
-                variant="contained"
-                className={classes['mt-6']}
-              >
-                Get Started
-              </Button>
-            </Link>
-
-            <Typography variant="subtitle2" className={classes['m-6']}>
-              Join the developers that are integrating API&#39;s around the world.
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-
-      <Container maxWidth="lg" className={classes.manage}>
-        <Grid container spacing={3}>
-          <Grid container item xs={12} justify="center">
-            <Typography variant="h6" component="h2" className={classes['mt-2']}>
-              Advantages of BridgeAPI
-            </Typography>
-          </Grid>
-
-          <Grid container item xs={12} alignItems="center" direction="column">
-            <Grid container item direction="row" justify="center">
-              <Card style={{ padding: 14 }}>
-                <Typography>Customize each workflow</Typography>
-                {/* SMALL TEXT: alter or complement the data
-                you want sent between apps */}
-              </Card>
-              <Card style={{ padding: 14 }}>
-                <Typography>Monitor all your webhooks in one place</Typography>
-                {/* SMALL TEXT: No more need to log in to every app to inspect
-                its webhook activity, it's all available from the BridgeAPI dashboard */}
-              </Card>
-            </Grid>
-            <Grid container item direction="row" justify="center">
-              <Card style={{ padding: 14 }}>
-                <Typography>Automate every workflow</Typography>
-                {/* SMALL TEXT: Once defined, Workflows are triggered and execute automatically */}
-              </Card>
-              <Card style={{ padding: 14 }}>
-                <Typography>Protect your privacy</Typography>
-                {/* SMALL TEXT: We do not ask for any permissions
-                to reach into your connected apps With bridgeAPI as the go-between,
-                your connected apps can gather only limited data on your private workflows */}
-              </Card>
-            </Grid>
-            {/*
-            <Typography variant="subtitle1">
-              Maybe a 2 x 2 grid with some icons and light text on the right side.
-            </Typography> */}
-            {/* <Typography variant="subtitle1">
-              dont ask me what to do for this box. I am out of ideas.
-            </Typography> */}
-
-            <Typography variant="subtitle2" className={classes['mt-6']}>
-              Some small test here
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-
-      <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid container item xs={12} justify="center">
-            <Typography variant="h5" component="h2" className={classes['m-4']}>
-              Some Call To Action
-            </Typography>
-
-            <Link href="/users/signup">
-              <Button
-                color="primary"
-                variant="contained"
-                className={classes['m-4']}
-              >
-                Get Started Now
-              </Button>
+              <Button className={classes.button}>Sign Up Now</Button>
             </Link>
           </Grid>
+          <img style={{ display: 'block', maxWidth: '100vw' }} src={dashboard} alt="header" />
+        </Grid>
+        <Grid container>
+          <Grid container className={classes.paddedContainer}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4" className={classes.header}>Fully customizable requests</Typography>
+              <Typography variant="body1" className={classes.mainText}>
+                BridgeAPI gives developers the ability to connect APIs easily,
+                regardless of any differences. One of those differences being
+                the headers they expect. You&apos;re able to easily define any headers needed
+                quickly with our service, even ones that must be generated
+                dynamically.
+                <br />
+                <br />
+                You can access data from the payload sent to your bridge
+                using
+                {' '}
+                <code>$payload.key_name</code>
+                , and your environment variables with
+                {' '}
+                <code>$env.key_name</code>
+                .
+                This allows you to create dynamic headers without issue.
+                <br />
+                <br />
+                We give users the flexibility they need, so they
+                can focus on what matters most: productivity.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <img style={{ width: '100%', height: '100%' }} src={headers} alt="header" />
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} className={classes.blue}>
+            <Grid item xs={12} md={6} className={classes.environmentVariablesImage}>
+              <img style={{ width: '100%', height: '100%' }} src={environmentVariables} alt="header" />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4" className={classes.header}>Keep your information safe</Typography>
+              <Typography variant="body1" className={classes.mainText}>
+                Your security & privacy are important to us. When you create an
+                environment variable, it is encrypted and hidden from view to
+                keep it safe.
+                <br />
+                <br />
+                Once an environment variable has been created, you&apos;ll be able to
+                access it in the headers & settings or payload sections. For
+                example, if you created an environment variable named
+                {' '}
+                <code>API_KEY</code>
+                , you&apos;d be able to access its value with
+                {' '}
+                <code>$env.API_KEY</code>
+                .
+                <br />
+                <br />
+                BridgeAPI doesn&apos;t ask for permissions to your essential apps,
+                either. We strive to be as unintrusive as possible.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container className={classes.paddedContainer}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4" className={classes.header}>Bridge almost any API together</Typography>
+              <Typography variant="body1" className={classes.mainText}>
+                You must be able to send webhooks from service A to your bridge&apos;s URL. Service
+                B must then accept BridgeAPI&apos;s webhooks. This should allow any two services
+                to be bridged together, regardless if they were ever meant to be.
+                <br />
+                <br />
+                You&apos;ll then craft a JSON payload in the format expected by service B in this
+                section here. You&apos;re able to reference
+                values from service A&apos;s payload using
+                {' '}
+                <code>$payload </code>
+                {' '}
+                and the name of the key.
+                <br />
+                <br />
+                For example, if you need the value of the key first_name, you&apos;d write out
+                {' '}
+                <code>$payload.first_name.</code>
+                {' '}
+                You can also access your environment variables, in the same
+                way, using
+                {' '}
+                <code>$env</code>
+                .
+              </Typography>
+              <Link href="/users/signup">
+                <Button className={classes.button}>Try It Now</Button>
+              </Link>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <img style={{ width: '100%', height: '100%' }} src={payload} alt="header" />
+            </Grid>
+          </Grid>
         </Grid>
       </Container>
-
-      <Footer handleOpen={handleOpen} />
     </div>
   );
 }
