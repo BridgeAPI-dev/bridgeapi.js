9011dda0bc785e1a275e5db4a87d92795284efac
Andrew <awcrotwell@gmail.com>
2020-12-29 18:42:58 -0500

    Fix eslint

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = withImages({
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
+      // eslint-disable-next-line no-param-reassign
       config.node = {
         fs: 'empty',
       };
