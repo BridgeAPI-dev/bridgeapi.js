7f8d5b682f77c0bcdc5d3aa4846787b6beb4b732
Andrew <49383564+andrewc910@users.noreply.github.com>
2021-01-04 16:30:42 -0500

    Fix codemirror json linting & formatting (#85)
    * Fix codemirror json linting & formatting
    
    * Fix eslint

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 const withImages = require('next-images');
 
-module.exports = withImages();
+module.exports = withImages({
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      // eslint-disable-next-line no-param-reassign
+      config.node = {
+        fs: 'empty',
+      };
+    }
+
+    return config;
+  },
+});
